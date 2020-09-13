4d65650c97750bdf40d860f59993f70fa86e74a1
Nischal Hada <hadanischal@gmail.com>
2020-09-13 22:03:39 +1000

    [Initial Setup] Initial project setup

---
+++ WeatherCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherCombine
+//
+//  Created by Nischal Hada on 13/9/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
