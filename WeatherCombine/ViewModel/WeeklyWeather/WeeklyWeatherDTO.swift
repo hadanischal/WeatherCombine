2193774fc9cdf56d47a12bb2a71bd7fc4a71a537
Nischal Hada <hadanischal@gmail.com>
2020-09-23 20:54:45 +1000

    [DTO] Implemented WeeklyWeatherDTO

---
+++ WeatherCombine/ViewModel/WeeklyWeather/WeeklyWeatherDTO.swift
@@ -0,0 +1,41 @@
+//
+//  WeeklyWeatherDTO.swift
+//  WeatherCombine
+//
+//  Created by Nischal Hada on 20/9/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - WeeklyWeatherDTO
+
+struct WeeklyWeatherDTO: Identifiable {
+    var id: String { day + temperature + title }
+    let day: String
+    let month: String
+    let temperature: String
+
+    let title: String
+    let fullDescription: String
+
+    init(withWeatherResult item: WeatherResult) {
+        self.day = dayFormatter.string(from: item.date)
+        self.month = monthFormatter.string(from: item.date)
+        self.temperature = item.main.temp.roundSinglePlace
+        self.title = item.weather.first?.main.rawValue ?? ""
+        self.fullDescription = item.weather.first?.weatherDescription ?? ""
+    }
+}
+
+// Used to hash on just the day in order to produce a single view model for each
+// day when there are multiple items per each day.
+extension WeeklyWeatherDTO: Hashable {
+    static func == (lhs: WeeklyWeatherDTO, rhs: WeeklyWeatherDTO) -> Bool {
+        return lhs.day == rhs.day
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.day)
+    }
+}
