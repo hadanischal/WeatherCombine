7f78ccb5e4c8027db710ff7ce543f30e121b5181
Nischal Hada <hadanischal@gmail.com>
2020-09-25 07:34:48 +1000

    [ViewModel] Implemented CurrentWeatherViewModel

---
+++ WeatherCombine/ViewModel/CurrentWeather/CurrentWeatherViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  CurrentWeatherViewModel.swift
+//  WeatherCombine
+//
+//  Created by Nischal Hada on 22/9/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+final class CurrentWeatherViewModel: ObservableObject {
+    @Published var dataSource: CurrentWeatherDTO?
+
+    let city: String
+    private let weatherFetcher: WeatherFetchable
+    private var disposables = Set<AnyCancellable>()
+
+    init(city: String, weatherFetcher: WeatherFetchable = WeatherNetworking()) {
+        self.weatherFetcher = weatherFetcher
+        self.city = city
+    }
+
+    func refresh() {
+        weatherFetcher
+            .currentWeatherForecast(forCity: city)
+            .map(CurrentWeatherDTO.init)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] value in
+                guard let self = self else { return }
+                switch value {
+                case .failure:
+                    self.dataSource = nil
+                case .finished:
+                    break
+                }
+                }, receiveValue: { [weak self] weather in
+                    guard let self = self else { return }
+                    self.dataSource = weather
+            })
+            .store(in: &disposables)
+    }
+}
