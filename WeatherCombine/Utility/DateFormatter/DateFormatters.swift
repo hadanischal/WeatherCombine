4a02c5f89f4df981324d43436ad659a1040febca
Nischal Hada <hadanischal@gmail.com>
2020-09-24 21:05:43 +1000

    [Refactored] Code Refactored

--- WeatherCombine/Utility/DateFormatter/DateFormatters.swift
+++ WeatherCombine/Utility/DateFormatter/DateFormatters.swift
@@ -9,13 +9,13 @@
 import Foundation
 
 let dayFormatter: DateFormatter = {
-  let formatter = DateFormatter()
-  formatter.dateFormat = "dd"
-  return formatter
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd"
+    return formatter
 }()
 
 let monthFormatter: DateFormatter = {
-  let formatter = DateFormatter()
-  formatter.dateFormat = "MMMM"
-  return formatter
+    let formatter = DateFormatter()
+    formatter.dateFormat = "MMMM"
+    return formatter
 }()
--- WeatherCombine/Utility/Extensions/Array Extensions/Array+Filtering.swift
+++ WeatherCombine/Utility/Extensions/Array Extensions/Array+Filtering.swift
@@ -10,8 +10,8 @@ import Foundation
 
 /// Taken from here: https://stackoverflow.com/a/46354989/491239
 public extension Array where Element: Hashable {
-  static func removeDuplicates(_ elements: [Element]) -> [Element] {
-    var seen = Set<Element>()
-    return elements.filter { seen.insert($0).inserted }
-  }
+    static func removeDuplicates(_ elements: [Element]) -> [Element] {
+        var seen = Set<Element>()
+        return elements.filter { seen.insert($0).inserted }
+    }
 }
