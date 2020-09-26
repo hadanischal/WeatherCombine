d56483f8c4352b45c5e843644cdbeacec5b42036
Nischal Hada <hadanischal@gmail.com>
2020-09-26 17:37:09 -0400

    [Builder] Implemented WeeklyWeatherBuilder

---
+++ WeatherCombine/Builder/WeeklyWeatherBuilder.swift
@@ -0,0 +1,16 @@
+//
+//  WeeklyWeatherBuilder.swift
+//  WeatherCombine
+//
+//  Created by Nischal Hada on 20/9/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import SwiftUI
+
+enum WeeklyWeatherBuilder {
+    static func makeCurrentWeatherView(withCity city: String) -> some View {
+        let viewModel = CurrentWeatherViewModel(city: city)
+        return CurrentWeatherView(viewModel: viewModel)
+    }
+}
