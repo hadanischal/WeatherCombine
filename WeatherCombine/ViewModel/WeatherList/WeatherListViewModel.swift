9bffb0112e30e432a0133e7bf7ad4894e2c52cd0
Nischal Hada <hadanischal@gmail.com>
2020-10-06 21:11:00 +1100

    [ViewModel] Implemented WeatherListViewModel

---
+++ WeatherCombine/ViewModel/WeatherList/WeatherListViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  WeatherListViewModel.swift
+//  WeatherCombine
+//
+//  Created by Nischal Hada on 4/10/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+final class WeatherListViewModel: ObservableObject {
+    @Published var cityIDs: [String] = OpenWeatherAPI.CityIDs
+    @Published var dataSource: [WeatherListDTO] = []
+
+    private let weatherFetcher: WeatherListFetchable
+    private var disposables = Set<AnyCancellable>()
+
+    init(
+        weatherFetcher: WeatherListFetchable = WeatherNetworking(),
+        scheduler: DispatchQueue = DispatchQueue(label: "WeatherListViewModel")
+    ) {
+        self.weatherFetcher = weatherFetcher
+        $cityIDs
+            .sink(receiveValue: fetchWeather(forCities:))
+            .store(in: &disposables)j
+    }
+
+    func fetchWeather(forCities cities: [String]) {
+        weatherFetcher
+            .getCurrentWeather(forCities: OpenWeatherAPI.CityIDs)
+            .map { response in
+                response.map(WeatherListDTO.init)
+        }
+        .map(Array.removeDuplicates)
+        .receive(on: DispatchQueue.main)
+        .sink(
+            receiveCompletion: { [weak self] value in
+                guard let self = self else { return }
+                switch value {
+                case .failure:
+                    self.dataSource = []
+                case .finished:
+                    break
+                }
+            },
+            receiveValue: { [weak self] forecast in
+                guard let self = self else { return }
+                self.dataSource = forecast
+            }
+        )
+        .store(in: &disposables)
+    }
+}
