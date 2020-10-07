0696098565184f86d43b13cb78f84776c1c13459
Nischal Hada <hadanischal@gmail.com>
2020-10-07 22:13:49 +1100

    [SceneDelegate] Updated SceneDelegate

--- WeatherCombine/SceneDelegate.swift
+++ WeatherCombine/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
 
-        let contentView = WeeklyWeatherView()
+        let contentView = WeatherListView()
         // Use a UIHostingController as window root view controller.
         guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
